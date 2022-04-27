71dfc75df42bf4b6a3c9131fdf46065668e8c753
VMLujanJr <vmlujanjr@outlook.com>
2022-04-27 00:09:25 -0500

    created getInventory & getStat prototype methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -18,7 +18,7 @@ test('creates a player object', () => {
 
     // get Stats
     expect(player.name).toBe('Victor');
-    expect(player.name.length).toBeGreaterThan(0);
+    expect(player.name.length).toBeGreaterThan(0); // doesn't have to be here... kinda just wrote it
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
@@ -29,4 +29,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test('get player\'s stats as an object', () => {
+    const player = new Player('Jr.');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get player\'s inventory as an object', () => {
+    const player = new Player('Ivette');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
